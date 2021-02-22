89e4d8e21c229ffd0ae91194b2afcfd69b990045
FerreiraEkl <36898301+FerreiraEkl@users.noreply.github.com>
2021-02-22 00:50:11 -0300

    Update configS3.ts

--- backEnd/src/config/configS3.ts
+++ backEnd/src/config/configS3.ts
@@ -1,8 +1,8 @@
 import aws from 'aws-sdk'
 
 export default new aws.S3({
-    accessKeyId: 'AKIAUTKVQMYVKYIWIZX7',
-    secretAccessKey: '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',
+    accessKeyId: 'hh',
+    secretAccessKey: 'gg',
     region: 'sa-east-1',
     sslEnabled: false//process.env.USE_SSL ? true : false
 });
